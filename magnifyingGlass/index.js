42eb865c0160296f76dc7da9adc06514c78ed3e5
Mathis Stone <thepsychedelliclucario@Mathiss-MacBook-Pro.local>
2022-10-12 18:44:29 -0400

    mag glass finish

--- magnifyingGlass/index.js
+++ magnifyingGlass/index.js
@@ -1,8 +1,6 @@
-const larger = document.querySelector("button")
-larger.addEventListener("click",function(){
+document.querySelector(".larger").addEventListener("click",function(){
 paragraph.style.fontSize = "98px"
 })
-const smaller = document.querySelector("button")
-smaller.addEventListener("click",function(){
+document.querySelector(".smaller").addEventListener("click",function(){
 paragraph.style.fontSize = "5px"
 })
