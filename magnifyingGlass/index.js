e67fbef8e3a8dcfb20a9c16711feb46482cb4f3a
Mathis Stone <thepsychedelliclucario@Mathiss-MacBook-Pro.local>
2022-10-12 18:34:33 -0400

    mag workip

---
+++ magnifyingGlass/index.js
@@ -0,0 +1,8 @@
+const larger = document.querySelector("button")
+larger.addEventListener("click",function(){
+paragraph.style.fontSize = "98px"
+})
+const smaller = document.querySelector("button")
+smaller.addEventListener("click",function(){
+paragraph.style.fontSize = "5px"
+})
