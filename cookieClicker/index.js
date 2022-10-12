c498f2503bd09b84487e57798c12c75431af34b8
jmealor <jmealor@icloud.com>
2022-10-12 17:32:42 -0400

    successful cookieClicker

---
+++ cookieClicker/index.js
@@ -0,0 +1,17 @@
+const button = document.querySelector("button");
+cookieimg.addEventListener("click", function () {
+// select p element and store in a variable
+const pElem = document.getElementById('clickCount');
+// grab the inner text of p element
+pElem.innerText = parseInt(pElem.innerText) + 1;
+// convert str to num
+// add 1
+// update html
+// yay!
+console.log(clickCount);
+});
+
+
+
+
+
